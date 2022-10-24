2b615001a6eb414aa5cf9ec71e2aebbe038466a6
artemiosdev <flyboroda@gmail.com>
2022-10-24 12:09:36 +0300

    Add new folder

---
+++ small apps, examples/WeeklyFinderArt/WeeklyFinderArt/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  WeeklyFinderArt
+//
+//  Created by Artem Androsenko on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text  else { return }
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        // get the date
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "EEEE"
+        
+        // date output
+        guard let date = calendar.date(from: dateComponents) else { return }
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        resultLabel.text = capitalizedWeekday
+    }
+    
+    // keyboard setup hidden/show
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        resultLabel.text = ""
+    }
+}
