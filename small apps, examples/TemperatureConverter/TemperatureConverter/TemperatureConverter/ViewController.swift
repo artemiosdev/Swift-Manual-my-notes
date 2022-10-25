af735887e2e7c538d487751bec17d5b78d230c7c
artemiosdev <flyboroda@gmail.com>
2022-10-25 21:52:06 +0300

    Add TemperatureConverter with UISlider

--- small apps, examples/TemperatureConverter/TemperatureConverter/TemperatureConverter/ViewController.swift
+++ small apps, examples/TemperatureConverter/TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -13,12 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-        
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32 ))
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
     }
 }
 
