4ffc7de4c20f61849781481bc66f868026a922cb
artemiosdev <flyboroda@gmail.com>
2022-11-05 22:57:46 +0300

    Add new app the ArtCover with UITableView

---
+++ small apps, examples/ArtCover/ArtCover/TrackList.swift
@@ -0,0 +1,107 @@
+//
+//  TrackList.swift
+//  ArtCover
+//
+//  Created by Artem Androsenko on 05.11.2022.
+//
+
+import UIKit
+
+class TrackList: UITableViewController {
+    
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 90
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    // MARK: - Table view data source
+    // возвращает кол-во строк
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return imageNameArray.count
+    }
+    
+    // создание ячейки по Identifier и работа с ячейкой
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Title", for: indexPath)
+//        cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+//        cell.textLabel?.text = imageNameArray[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        
+        // Configure content.
+        content.image = UIImage(named: imageNameArray[indexPath.row])
+        content.text = imageNameArray[indexPath.row]
+        content.textProperties.numberOfLines = 0
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 80
+//    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- small apps, examples/README.md
+++ small apps, examples/README.md
@@ -4,17 +4,25 @@
 
 <img alt="gif" src="images/WeeklyFinderArt.gif" height = 510 width = 233 />
 
+---
+
 - **[TemperatureConverter](https://github.com/artemiosdev/Swift-Manual-my-notes/tree/main/small%20apps%2C%20examples/TemperatureConverter/TemperatureConverter/TemperatureConverter)** - используется [UISlider](https://developer.apple.com/documentation/uikit/uislider), округление [round()](https://www.advancedswift.com/rounding-floats-and-doubles-in-swift/) 
 
 <img alt="gif" src="images/TemperatureConverter.gif" height = 510 width = 233 />
+
+---
 
 - **[PassDataProjectArt](https://github.com/artemiosdev/Swift-Manual-my-notes/tree/main/small%20apps%2C%20examples/PassDataProjectArt/PassDataProjectArt)** - UITextField c login и password, выбор segue  учитывая идентификатор [performSegue(withIdentifier:, sender:)](https://developer.apple.com/documentation/uikit/uiviewcontroller/1621413-performsegue), возврат segue на другой экран с помощью  unwind Segue реализуется как к элементом, так и с View (предпочтительнее). Взаимодействие с [UIStoryboardSegue](https://developer.apple.com/documentation/uikit/uistoryboardsegue/) (destination, source, identifier). Передача данных с одного View на другой, [prepare(for segue: UIStoryboardSegue, sender: )](https://developer.apple.com/documentation/uikit/uiviewcontroller/1621490-prepare) этот метод уведомляет view controller о том, что переход вот-вот будет выполнен. . Взаимодействие с клавиатурой [touchesBegan()](https://developer.apple.com/documentation/uikit/uiresponder/1621142-touchesbegan/) - сообщает этому объекту, что в представлении или окне произошло одно или несколько новых касаний.
 
 <img alt="gif" src="images/PassDataProjectArt.gif" height = 510 width = 233 />
 
+---
+
 - **[UILabelAndUIButton](https://github.com/artemiosdev/Swift-Manual-my-notes/tree/main/small%20apps%2C%20examples/UILabel/UILabel)**, пример использования [UIButton](https://developer.apple.com/documentation/uikit/uibutton) и [UILabel](https://developer.apple.com/documentation/uikit/uilabel), свойства элементов, верстка кодом `(isHidden, font, text, setTitle, titleLabel?.text, textColor, setTitleColor, backgroundColor)`
 
 <img alt="gif" src="images/UILabelAndUIButton.gif" height = 510 width = 233 />
+
+---
 
 - **[UISegmentedControl, UISlider, UITextField, UIScrollView, UIDatePicker, UISwitch, UIPickerView](https://github.com/artemiosdev/Swift-Manual-my-notes/tree/main/small%20apps%2C%20examples/UISegmentedControl/UISegmentedControl)**, пример использования [UISegmentedControl](https://developer.apple.com/documentation/uikit/uisegmentedcontrol), добавить кодом segment [segmentedContol.insertSegment](https://developer.apple.com/documentation/uikit/uisegmentedcontrol/1618588-insertsegment).
 
@@ -45,6 +53,8 @@ View, которое позволяет прокручивать и масшта
 
 <img alt="gif" src="images/UISegmentedControl.gif" height = 510 width = 233 />
 
+---
+
 - **[UITextView, UIStepper, UIActivityIndicatorView, UIProgressView](https://github.com/artemiosdev/Swift-Manual-my-notes/tree/main/small%20apps%2C%20examples/UITextView/UITextView)**, пример использования [UITextView ](https://developer.apple.com/documentation/uikit/uitextview)
 
 [UITextViewDelegate](https://developer.apple.com/documentation/uikit/uitextviewdelegate) - протокол включающий в себя методы отслеживания тапов, и не только. 
@@ -56,6 +66,3 @@ View, которое позволяет прокручивать и масшта
 [UIProgressView](https://developer.apple.com/documentation/uikit/uiprogressview) - индикатор прогресса загрузки.
 
 <img alt="gif" src="images/UITextView.gif" height = 510 width = 233 />
-
-
-
