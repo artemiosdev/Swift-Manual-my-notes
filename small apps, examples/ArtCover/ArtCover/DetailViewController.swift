336e548ad38942b0761eef61c010362af2d51cd5
artemiosdev <flyboroda@gmail.com>
2022-11-06 12:28:20 +0300

    Добавил новый DetailViewController, переход между view, navigation controller

--- small apps, examples/ArtCover/ArtCover/DetailViewController.swift
+++ small apps, examples/ArtCover/ArtCover/DetailViewController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var songLabel: UILabel!
+    
+    var track: Track?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        image.image = UIImage(named: track?.title ?? "Image cover track")
+        artistLabel.text = track?.artist
+        songLabel.text = track?.song
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- small apps, examples/ArtCover/ArtCover/TrackList.swift
+++ small apps, examples/ArtCover/ArtCover/TrackList.swift
@@ -8,18 +8,13 @@
 import UIKit
 
 class TrackList: UITableViewController {
-    
     let imageNameArray = Track.getTrackList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 90
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -32,11 +27,11 @@ class TrackList: UITableViewController {
     // создание ячейки по Identifier и работа с ячейкой
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Title", for: indexPath)
-//        cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
-//        cell.textLabel?.text = imageNameArray[indexPath.row]
+        // устаревшие свойства
+        // cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        // cell.textLabel?.text = imageNameArray[indexPath.row]
         
         var content = cell.defaultContentConfiguration()
-        
         let track = imageNameArray[indexPath.row]
         // Configure content.
         content.image = UIImage(named: track.title)
@@ -48,53 +43,22 @@ class TrackList: UITableViewController {
         return cell
     }
     
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 80
-//    }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let detailViewController = segue.destination as! DetailViewController
+                let track = imageNameArray[indexPath.row]
+                detailViewController.track = track
+            }
+        }
+        
+//        или тот же переход, но с guard
+//        guard let detailViewController = segue.destination as? DetailViewController else { return }
+//        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+//        let track = imageNameArray[indexPath.row]
+//        detailViewController.track = track
+    }
     
 }
