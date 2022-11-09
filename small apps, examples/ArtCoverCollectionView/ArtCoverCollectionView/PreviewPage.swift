eb2c5c68f06ec2c35d59043ed70f6da2e6a0fb27
artemiosdev <flyboroda@gmail.com>
2022-11-09 15:03:37 +0300

    Finish ArtCoverCollectionView example app. Set image cell and layout, add functional visualization

--- small apps, examples/ArtCoverCollectionView/ArtCoverCollectionView/PreviewPage.swift
+++ small apps, examples/ArtCoverCollectionView/ArtCoverCollectionView/PreviewPage.swift
@@ -7,82 +7,45 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "ArtCover"
 
 class PreviewPage: UICollectionViewController {
-
+    let imageNameArray = Track.getTrackList()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        //      настройка размеров ячейки
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        layout.itemSize = CGSize(width: 105, height: 105)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 20
+        collectionView.collectionViewLayout = layout
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return imageNameArray.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCoverCell
+        let track = imageNameArray[indexPath.row]
+        //      Configure content.
+        cell.coverImageView.image = UIImage(named: track.title)
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
 }
---
+++ small apps, examples/ArtCoverCollectionView/ArtCoverCollectionView/Track.swift
@@ -0,0 +1,33 @@
+//
+//  Track.swift
+//  ArtCoverCollectionView
+//
+//  Created by Artem Androsenko on 09.11.2022.
+//
+
+import Foundation
+struct Track {
+    let artist: String
+    let song: String
+    
+    var title: String {
+        "\(artist) - \(song)"
+    }
+}
+
+extension Track {
+    static func getTrackList() -> [Track] {
+        [
+            Track(artist: "Alberto Ruiz", song: "7 Elements (Original Mix)"),
+            Track(artist: "Dave Wincent", song: "Red Eye (Original Mix)"),
+            Track(artist: "E-Spectro", song: "End Station (Original Mix)"),
+            Track(artist: "Edna Ann", song: "Phasma (Konstantin Yoodza Remix)"),
+            Track(artist: "Ilija Djokovic", song: "Delusion (Original Mix)"),
+            Track(artist: "John Baptiste", song: "Mycelium (Original Mix)"),
+            Track(artist: "Lane 8", song: "Fingerprint (Original Mix)"),
+            Track(artist: "Mac Vaughn", song: "Pink Is My Favorite Color (Alex Stein Remix)"),
+            Track(artist: "Metodi Hristov, Gallya", song: "Badmash (Original Mix)"),
+            Track(artist: "Veerus, Maxie Devine", song: "Nightmare (Original Mix)")
+        ]
+    }
+}
